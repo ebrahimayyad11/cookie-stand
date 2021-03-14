32c2fb9b21dd9d311b4fd5a7e7a6b5604316668b
ebrahim ayyad <ayyad_ebrahim@hotmail.com>
2021-03-14 02:53:11 +0200

    doing lab-08b and lab-10b

---
+++ js/home.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let white = confirm('Do you want to change the background color into white?');
+let body = document.getElementById('body');
+
+if (white = true){
+    body = document.style
+}
